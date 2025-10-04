48ddf9e8da06a7033c33bb55e1575db7d6fd493d
Andrew Reinhart <ndrurei@gmail.com>
2025-10-04 19:44:52 +0700

    add DarkMode

--- script/home.js
+++ script/home.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    document.body.classList.remove("light-mode");
+  } else {
+    document.body.classList.add("light-mode");
+    document.body.classList.remove("dark-mode");
+  }
+
+  // === Menu hamburger ===
   const hamburgerButton = document.querySelector(".hamburger-menu");
   const mobileMenu = document.querySelector(".mobile-nav");
 
