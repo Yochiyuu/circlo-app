97079063a20af0caae88a0407cfe7be1610fabef
Daniel <danielpbb06@gmail.com>
2025-10-08 20:44:43 +0700

    update community

---
+++ script/community.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+  }
+});
