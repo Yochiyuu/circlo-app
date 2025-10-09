4d8cfbbbe88e5a8c1b43452d81cd663cf52cc270
Daniel <danielpbb06@gmail.com>
2025-10-09 23:28:10 +0700

    update community

--- script/community.js
+++ script/community.js
@@ -3,4 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
   if (savedTheme === "dark") {
     document.body.classList.add("dark-mode");
   }
+
+  const hamburgerButton = document.querySelector(".hamburger-menu");
+  const mobileMenu = document.querySelector(".mobile-nav");
+
+  if (hamburgerButton && mobileMenu) {
+    hamburgerButton.addEventListener("click", () => {
+      hamburgerButton.classList.toggle("active");
+      mobileMenu.classList.toggle("open");
+    });
+  }
 });
